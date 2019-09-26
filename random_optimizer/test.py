09f9a7388a984b46ede0e7b8bc1f48aa46bb34ea
Hskby <trolgorn@gmail.com>
2019-09-26 16:28:13 +0300

    update tests

--- random_optimizer/test.py
+++ random_optimizer/test.py
@@ -3,22 +3,22 @@ from optimize import optimize
 
 
 
-def f1(x, y):
-    return x + y
+def f1(x):
+    return x[0] + x[1]
 
-def f2(x, y):
-    return x
+def f2(x):
+    return x[0]
 
 def check_optimizer(f):
     def check(a, b):
-        return f(a[0], a[1]) > f(b[0], b[1])
+        return f(a) > f(b)
     
     
     a = optimize(f)
     for i in range(len(a) - 1):
         if not check(a[i], a[i + 1]):
             raise ValueError("something is wrong")
-    if f(a[0][0], a[0][1]) - f(a[-1][0], a[-1][1]) <= 5:
+    if f(a[0]) - f(a[-1]) <= 5:
         raise ValueError("it is a bad optimizer, Mitya luzer")
     
 check_optimizer(f1)
